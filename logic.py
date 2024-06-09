08bc64f0841d0ca7a45adecda3c9895ec4145daf
unpublished3 <chapagainsakshyam@gmail.com>
2024-06-09 07:43:55 +0545

    Check for board terminal

--- logic.py
+++ logic.py
@@ -9,3 +9,73 @@ def turn(board):
             elif cell.get() == "O":
                 o_count += 1
     return "X" if x_count == o_count else "O"
+
+
+def terminal(board):
+    if check_win(board, "X") or check_draw(board):
+        return True
+    return False
+
+
+def check_win(board):
+    for piece in ["X", "O"]:
+        if (
+            check_diagonal(board, piece)
+            or check_horizontal(board, piece)
+            or check_vertical(board, piece)
+        ):
+            return True
+
+    return False
+
+
+def check_horizontal(board, piece):
+    for i in range(3):
+        if (
+            board[i][0].get() == piece
+            and board[i][1].get() == piece
+            and board[i][2].get() == piece
+        ):
+            return True
+    return False
+
+
+def check_vertical(board, piece):
+    for i in range(3):
+        if (
+            board[0][i].get() == piece
+            and board[1][i].get() == piece
+            and board[1][i].get() == piece
+        ):
+            return True
+    return False
+
+
+def check_diagonal(board, piece):
+    if (
+        board[0][0].get() == piece
+        and board[1][1].get() == piece
+        and board[2][2].get() == piece
+    ):
+        return True
+
+    if (
+        board[0][2].get() == piece
+        and board[1][1].get() == piece
+        and board[2][0].get() == piece
+    ):
+        return True
+
+    return False
+
+
+def check_draw(board):
+    count = 0
+    for row in board:
+        for cell in row:
+            if cell.get() == "X" or cell.get() == "O":
+                count += 1
+
+    if count == 9:
+        return True
+    return False
